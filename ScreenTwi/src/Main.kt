02bb5c366e6e2b15d0c7bad611aa7117633bc83d
Kazuki Chigita <kazuki.chigichan@gmail.com>
2018-02-14 17:01:08 +0900

    First commit.

---
+++ ScreenTwi/src/Main.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by chigichan24 on 2018/02/14.
+ */
+
+class Main{
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("hello")
+        }
+    }
+}
